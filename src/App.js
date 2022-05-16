7b877d6a9b5f2d4ec021905d3891040fe998bf32
Haris Pasic <harispasic@ymail.com>
2022-05-16 06:21:48 +0200

    redux-songs-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import SongDetail from "./components/SongDetail";
+import SongList from "./components/SongList";
+
+const App = () => {
+  return (
+    <div>
+      <div className="left">
+        <SongList />
+      </div>
+      <div className="right panel">
+        <SongDetail />
+      </div>
+    </div>
+  );
+};
+
+export default App;
